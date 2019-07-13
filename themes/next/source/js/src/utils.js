4a711befa45551c1753a1a6644462a5d6d8b452f
daief <1437931235@qq.com>
2019-07-13 11:35:06 +0800

    feat: 添加文章图片加载失败时显示占位图

--- themes/next/source/js/src/utils.js
+++ themes/next/source/js/src/utils.js
@@ -4,25 +4,37 @@ NexT.utils = NexT.$u = {
   /**
    * Wrap images with fancybox support.
    */
-  wrapImageWithFancyBox: function () {
+  wrapImageWithFancyBox: function() {
     $('.content img')
       .not('[hidden]')
       .not('.group-picture img, .post-gallery img')
-      .each(function () {
+      .each(function() {
         var $image = $(this);
+
+        // 图片加载失败时显示占位图
+        $image.on('error', function() {
+          $image.attr('src', '/images/image-error.jpg');
+        });
+
         var imageTitle = $image.attr('title');
         var $imageWrapLink = $image.parent('a');
 
         if ($imageWrapLink.size() < 1) {
-	        var imageLink = ($image.attr('data-original')) ? this.getAttribute('data-original') : this.getAttribute('src');
-          $imageWrapLink = $image.wrap('<a href="' + imageLink + '"></a>').parent('a');
+          var imageLink = $image.attr('data-original')
+            ? this.getAttribute('data-original')
+            : this.getAttribute('src');
+          $imageWrapLink = $image
+            .wrap('<a href="' + imageLink + '"></a>')
+            .parent('a');
         }
 
         $imageWrapLink.addClass('fancybox fancybox.image');
         $imageWrapLink.attr('rel', 'group');
 
         if (imageTitle) {
-          $imageWrapLink.append('<p class="image-caption">' + imageTitle + '</p>');
+          $imageWrapLink.append(
+            '<p class="image-caption">' + imageTitle + '</p>',
+          );
 
           //make sure img title tag will show correctly in fancybox
           $imageWrapLink.attr('title', imageTitle);
@@ -32,24 +44,26 @@ NexT.utils = NexT.$u = {
     $('.fancybox').fancybox({
       helpers: {
         overlay: {
-          locked: false
-        }
-      }
+          locked: false,
+        },
+      },
     });
   },
 
-  lazyLoadPostsImages: function () {
-    $('#posts').find('img').lazyload({
-      //placeholder: '/images/loading.gif',
-      effect: 'fadeIn',
-      threshold : 0
-    });
+  lazyLoadPostsImages: function() {
+    $('#posts')
+      .find('img')
+      .lazyload({
+        //placeholder: '/images/loading.gif',
+        effect: 'fadeIn',
+        threshold: 0,
+      });
   },
 
-  registerESCKeyEvent: function () {
-    $(document).on('keyup', function (event) {
-      var shouldDismissSearchPopup = event.which === 27 &&
-        $('.search-popup').is(':visible');
+  registerESCKeyEvent: function() {
+    $(document).on('keyup', function(event) {
+      var shouldDismissSearchPopup =
+        event.which === 27 && $('.search-popup').is(':visible');
       if (shouldDismissSearchPopup) {
         $('.search-popup').hide();
         $('.search-popup-overlay').remove();
@@ -58,31 +72,39 @@ NexT.utils = NexT.$u = {
     });
   },
 
-  registerBackToTop: function () {
+  registerBackToTop: function() {
     var THRESHOLD = 50;
     var $top = $('.back-to-top');
-    var $toggleIndex = $('.toggle-index');  // daief
+    var $toggleIndex = $('.toggle-index'); // daief
 
-    $(window).on('scroll', function () {
+    $(window).on('scroll', function() {
       $top.toggleClass('back-to-top-on', window.pageYOffset > THRESHOLD);
-      $toggleIndex.toggleClass('toggle-index-on', window.pageYOffset > THRESHOLD + 48); // daief
+      $toggleIndex.toggleClass(
+        'toggle-index-on',
+        window.pageYOffset > THRESHOLD + 48,
+      ); // daief
 
       var scrollTop = $(window).scrollTop();
       var docHeight = $('#content').height();
       var winHeight = $(window).height();
-      var contentMath = (docHeight > winHeight) ? (docHeight - winHeight) : ($(document).height() - winHeight);
-      var scrollPercent = (scrollTop) / (contentMath);
-      var scrollPercentRounded = Math.round(scrollPercent*100);
-      var scrollPercentMaxed = (scrollPercentRounded > 100) ? 100 : scrollPercentRounded;
+      var contentMath =
+        docHeight > winHeight
+          ? docHeight - winHeight
+          : $(document).height() - winHeight;
+      var scrollPercent = scrollTop / contentMath;
+      var scrollPercentRounded = Math.round(scrollPercent * 100);
+      var scrollPercentMaxed =
+        scrollPercentRounded > 100 ? 100 : scrollPercentRounded;
       $('#scrollpercent>span').html(scrollPercentMaxed);
     });
 
-    $top.on('click', function () {
+    $top.on('click', function() {
       $('body').velocity('scroll');
     });
 
-    $toggleIndex.on('click', function () {  // daief
-      $('.sidebar').toggleClass('sidebar-on')
+    $toggleIndex.on('click', function() {
+      // daief
+      $('.sidebar').toggleClass('sidebar-on');
     });
   },
 
@@ -90,7 +112,7 @@ NexT.utils = NexT.$u = {
    * Transform embedded video to support responsive layout.
    * @see http://toddmotto.com/fluid-and-responsive-youtube-and-vimeo-videos-with-fluidvids-js/
    */
-  embeddedVideoTransformer: function () {
+  embeddedVideoTransformer: function() {
     var $iframes = $('iframe');
 
     // Supported Players. Extend this if you need more players.
@@ -99,30 +121,33 @@ NexT.utils = NexT.$u = {
       'player.vimeo.com',
       'player.youku.com',
       'music.163.com',
-      'www.tudou.com'
+      'www.tudou.com',
     ];
-    var pattern = new RegExp( SUPPORTED_PLAYERS.join('|') );
+    var pattern = new RegExp(SUPPORTED_PLAYERS.join('|'));
 
-    $iframes.each(function () {
+    $iframes.each(function() {
       var iframe = this;
       var $iframe = $(this);
       var oldDimension = getDimension($iframe);
       var newDimension;
 
       if (this.src.search(pattern) > 0) {
-
         // Calculate the video ratio based on the iframe's w/h dimensions
-        var videoRatio = getAspectRadio(oldDimension.width, oldDimension.height);
+        var videoRatio = getAspectRadio(
+          oldDimension.width,
+          oldDimension.height,
+        );
 
         // Replace the iframe's dimensions and position the iframe absolute
         // This is the trick to emulate the video ratio
-        $iframe.width('100%').height('100%')
+        $iframe
+          .width('100%')
+          .height('100%')
           .css({
             position: 'absolute',
             top: '0',
-            left: '0'
+            left: '0',
           });
-
 
         // Wrap the iframe in a new <div> which uses a dynamically fetched padding-top property
         // based on the video's w/h dimensions
@@ -141,12 +166,14 @@ NexT.utils = NexT.$u = {
         // Additional adjustments for 163 Music
         if (this.src.search('music.163.com') > 0) {
           newDimension = getDimension($iframe);
-          var shouldRecalculateAspect = newDimension.width > oldDimension.width ||
-                                        newDimension.height < oldDimension.height;
+          var shouldRecalculateAspect =
+            newDimension.width > oldDimension.width ||
+            newDimension.height < oldDimension.height;
 
           // 163 Music Player has a fixed height, so we need to reset the aspect radio
           if (shouldRecalculateAspect) {
-            wrap.style.paddingTop = getAspectRadio(newDimension.width, oldDimension.height) + '%';
+            wrap.style.paddingTop =
+              getAspectRadio(newDimension.width, oldDimension.height) + '%';
           }
         }
       }
@@ -155,12 +182,12 @@ NexT.utils = NexT.$u = {
     function getDimension($element) {
       return {
         width: $element.width(),
-        height: $element.height()
+        height: $element.height(),
       };
     }
 
     function getAspectRadio(width, height) {
-      return height / width * 100;
+      return (height / width) * 100;
     }
   },
 
@@ -168,13 +195,15 @@ NexT.utils = NexT.$u = {
    * Add `menu-item-active` class name to menu item
    * via comparing location.path with menu item's href.
    */
-  addActiveClassToMenuItem: function () {
+  addActiveClassToMenuItem: function() {
     var path = window.location.pathname;
     path = path === '/' ? path : path.substring(0, path.length - 1);
-    $('.menu-item a[href^="' + path + '"]:first').parent().addClass('menu-item-active');
+    $('.menu-item a[href^="' + path + '"]:first')
+      .parent()
+      .addClass('menu-item-active');
   },
 
-  hasMobileUA: function () {
+  hasMobileUA: function() {
     var nav = window.navigator;
     var ua = nav.userAgent;
     var pa = /iPad|iPhone|Android|Opera Mini|BlackBerry|webOS|UCWEB|Blazer|PSP|IEMobile|Symbian/g;
@@ -182,15 +211,19 @@ NexT.utils = NexT.$u = {
     return pa.test(ua);
   },
 
-  isTablet: function () {
-    return window.screen.width < 992 && window.screen.width > 767 && this.hasMobileUA();
+  isTablet: function() {
+    return (
+      window.screen.width < 992 &&
+      window.screen.width > 767 &&
+      this.hasMobileUA()
+    );
   },
 
-  isMobile: function () {
+  isMobile: function() {
     return window.screen.width < 767 && this.hasMobileUA();
   },
 
-  isDesktop: function () {
+  isDesktop: function() {
     return !this.isTablet() && !this.isMobile();
   },
 
@@ -200,27 +233,29 @@ NexT.utils = NexT.$u = {
    * @param selector
    * @returns {string|void|XML|*}
    */
-  escapeSelector: function (selector) {
+  escapeSelector: function(selector) {
     return selector.replace(/[!"$%&'()*+,.\/:;<=>?@[\\\]^`{|}~]/g, '\\$&');
   },
 
-  displaySidebar: function () {
+  displaySidebar: function() {
     if (!this.isDesktop() || this.isPisces()) {
       return;
     }
     $('.sidebar-toggle').trigger('click');
   },
 
-  isMist: function () {
+  isMist: function() {
     return CONFIG.scheme === 'Mist';
   },
 
-  isPisces: function () {
+  isPisces: function() {
     return CONFIG.scheme === 'Pisces';
   },
 
-  getScrollbarWidth: function () {
-    var $div = $('<div />').addClass('scrollbar-measure').prependTo('body');
+  getScrollbarWidth: function() {
+    var $div = $('<div />')
+      .addClass('scrollbar-measure')
+      .prependTo('body');
     var div = $div[0];
     var scrollbarWidth = div.offsetWidth - div.clientWidth;
 
@@ -234,7 +269,7 @@ NexT.utils = NexT.$u = {
    *
    * @returns {Boolean}
    */
-  needAffix: function () {
+  needAffix: function() {
     return this.isPisces();
-  }
+  },
 };
