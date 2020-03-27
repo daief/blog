db9bd46d48eaef7c5c820e2658bd92976cfd6a97
daief <1437931235@qq.com>
2020-03-27 16:56:59 +0800

    style: refine hitokoto style

--- themes/next/source/js/daief/common.js
+++ themes/next/source/js/daief/common.js
@@ -24,6 +24,38 @@ setTimeout(() => {
   }
 
   /**
+   * 获得一个随机的颜色
+   */
+  function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  /**
+   * ref: https://stackoverflow.com/a/13532993
+   * @param {*} color
+   * @param {*} percent
+   */
+  function shadeColor(color, percent) {
+    var R = parseInt(color.substring(1, 3), 16);
+    var G = parseInt(color.substring(3, 5), 16);
+    var B = parseInt(color.substring(5, 7), 16);
+
+    R = parseInt((R * (100 + percent)) / 100);
+    G = parseInt((G * (100 + percent)) / 100);
+    B = parseInt((B * (100 + percent)) / 100);
+
+    R = R < 255 ? R : 255;
+    G = G < 255 ? G : 255;
+    B = B < 255 ? B : 255;
+
+    var RR = R.toString(16).length == 1 ? '0' + R.toString(16) : R.toString(16);
+    var GG = G.toString(16).length == 1 ? '0' + G.toString(16) : G.toString(16);
+    var BB = B.toString(16).length == 1 ? '0' + B.toString(16) : B.toString(16);
+
+    return '#' + RR + GG + BB;
+  }
+
+  /**
    * 获取每日一言
    */
   function getHitokoto() {
@@ -31,11 +63,37 @@ setTimeout(() => {
       return;
     }
     getHitokoto.loading = true;
+
+    /**
+     * 随机出圆角
+     */
+    var generateBorderRadius = function() {
+      var delta = Math.random() * 10 - 5;
+      var long = 220 + 2 * delta + 'px';
+      var short = 30 + delta + 'px';
+      return Math.random() * 100 > 50 ? long + ' ' + short : short + ' ' + long;
+    };
+
     $.get('https://v1.hitokoto.cn/?encode=json')
       .done(function(data) {
         if (data && data.hitokoto) {
-          var content = data.hitokoto;
-          $('#hitokoto .hko-content').text(content);
+          var content = data.hitokoto || '';
+
+          $('#hitokoto')
+            .addClass(`kto-br`)
+            .css({
+              borderTopLeftRadius: generateBorderRadius(),
+              borderTopRightRadius: generateBorderRadius(),
+              borderBottomRightRadius: generateBorderRadius(),
+              borderBottomLeftRadius: generateBorderRadius(),
+              borderColor: getRandomColor(),
+            });
+          $('#hitokoto .hko-content').html(
+            '<strong>' +
+              content.substring(0, 1) +
+              '</strong>' +
+              content.substring(1),
+          );
           $('#hitokoto .hko-author').html(
             '<span style="font-family:cursive;">—— </span>' + data.from,
           );
