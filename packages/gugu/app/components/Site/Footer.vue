de4bb2902121553187ca13809f0200859c677c3a
daief <1437931235@qq.com>
2021-07-30 23:19:13 +0800

    wip: jump to more

--- packages/gugu/app/components/Site/Footer.vue
+++ packages/gugu/app/components/Site/Footer.vue
@@ -13,10 +13,11 @@ export default defineComponent({
     const copyright = computed(
       () => `© ${site.blogConfig.since}-${new Date().getFullYear()}`,
     );
+    const author = computed(() => `By gugu & ${site.blogConfig.author}`);
     return () => (
       <div class="text-center text-sm">
         <div class="mb-2">{copyright.value}</div>
-        <div class="mb-2">By gugu & {site.blogConfig.author}</div>
+        <div class="mb-2">{author.value}</div>
         {store.state.global.site.site_pv ? (
           <div>本站总访问量 {store.state.global.site.site_pv}</div>
         ) : null}
