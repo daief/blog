80c89fed63f59196471d71972155b1573f8a7985
daief <1437931235@qq.com>
2021-07-30 23:30:42 +0800

    fix

--- packages/gugu/app/plugins/loadUserScripts.ts
+++ packages/gugu/app/plugins/loadUserScripts.ts
@@ -1,21 +1,27 @@
 export function loadUserScripts() {
   const modules = [
-    (import.meta as any).glob('../../dist/client/customRuntime/*.js'),
-    (import.meta as any).glob('../../dist/client/customRuntime/*.ts'),
-    (import.meta as any).glob('../../dist/client/customRuntime/*.less'),
-    (import.meta as any).glob('../../dist/client/customRuntime/*.css'),
+    (import.meta as any).glob('../../dist/client/customRuntime/*.@(t|j)s?(x)'),
+    // 样式懒加载有问题，同步引入
+    (import.meta as any).globEager(
+      '../../dist/client/customRuntime/*.@(le|c)ss',
+    ),
   ];
 
   for (const md of modules) {
     for (const path in md) {
       const displayPath = path.replace('../../dist/client/customRuntime', '');
-      md[path]()
-        .then(() => {
-          console.log(`Load [${displayPath}] Success`);
-        })
-        .catch((e) => {
-          console.warn(`Load [${displayPath}] Fail:`, e);
-        });
+      const moduleOrLoad = md[path];
+      if (typeof moduleOrLoad === 'function') {
+        moduleOrLoad()
+          .then(() => {
+            console.log(`Load [${displayPath}] Success`);
+          })
+          .catch((e) => {
+            console.warn(`Load [${displayPath}] Fail:`, e);
+          });
+      } else {
+        console.log(`Load [${displayPath}] Success`);
+      }
     }
   }
 }
