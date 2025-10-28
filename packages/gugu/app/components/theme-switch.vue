67e2c13283515db46efcd35ef62fa7574e795402
dddfff <1437931235@qq.com>
2025-10-28 20:05:50 +0800

    wip: refine theme switch

--- packages/gugu/app/components/theme-switch.vue
+++ packages/gugu/app/components/theme-switch.vue
@@ -1,13 +1,25 @@
 <template>
-  <button
-    @click="toggleTheme"
-    class="w-9 h-9 bg-gray-200 rounded-lg dark:bg-gray-600 flex items-center justify-center hover:ring-2 ring-gray-300 transition-all cursor-pointer"
-  >
-    <i-mdi-white-balance-sunny v-if="theme === 'light'" />
-    <i-mdi-moon-and-stars v-else />
+  <button @click="toggleTheme" class="transition-all cursor-pointer relative">
+    <!-- track -->
+    <div
+      class="w-10 h-6 rounded-3xl border-[3px] border-[#d1d5da] transition-all duration-300 ease-in-out dark:border-[#6E40C9]"
+    >
+      <!-- thumb -->
+      <div
+        :class="[
+          'absolute top-1/2 left-0 transform -translate-y-1/2 transition-all duration-300 ease-in-out',
+          'w-[28px] h-[28px] bg-[#2f363d] rounded-full flex justify-center items-center',
+          'dark:bg-[#6E40C9] dark:left-[calc(100%-28px)]',
+        ]"
+      >
+        <span class="text-[16px]" v-html="svgMoonHtml" />
+      </div>
+    </div>
   </button>
 </template>
 <script setup lang="ts">
 import { useTheme } from '@app/composables/use-theme';
-const { theme, toggleTheme } = useTheme();
+// @ts-ignore
+import svgMoonHtml from '@app/assets/moon.svg?raw';
+const { toggleTheme } = useTheme();
 </script>
