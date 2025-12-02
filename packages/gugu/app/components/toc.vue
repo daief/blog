0f0ec211d418ecd9c6aee6d22031e062e8d6df56
daief <1437931235@qq.com>
2025-12-02 22:41:18 +0800

    fix: toc style

--- packages/gugu/app/components/toc.vue
+++ packages/gugu/app/components/toc.vue
@@ -85,6 +85,12 @@ if (!import.meta.env.SSR) {
   });
 }
 
+/**
+ * 目录响应式原则
+ * - < lg(1024): 抽屉弹层，常驻显示
+ * - >= lg: sticky 在右侧
+ * - >= xl: 宽度更大
+ */
 const renderToc = (list?: ITocItem[], level = 0) => {
   if (!list?.length) return null;
   return (
@@ -95,41 +101,52 @@ const renderToc = (list?: ITocItem[], level = 0) => {
         },
       ]}
     >
-      {list.map((item) => (
-        <li>
-          <a
-            key={item.id}
-            href={`#${item.id}`}
-            class={[
-              'group/item text-foreground flex flex-nowrap items-center mb-1',
-            ]}
-          >
-            <span
+      {list.map((item) => {
+        const isActive = actives.value.includes(item.id);
+        return (
+          <li key={item.id}>
+            <a
+              key={item.id}
+              href={`#${item.id}`}
               class={[
-                'block h-1 bg-foreground rounded-sm opacity-30 transition-300 shrink-0',
-                'group-hover/item:opacity-85',
+                'group/item text-foreground flex flex-nowrap items-center mb-1.5',
               ]}
-              style={{
-                width: `${20 - level * 4}px`,
-                marginRight: `${32 - (6 - level) * 4}px`,
-              }}
-            />
-            <span
-              class={[
-                'text-[color-mix(in_srgb,var(--foreground)_70%,transparent)] text-sm overflow-hidden text-ellipsis whitespace-nowrap transition-300',
-                'max-w-46 flex-grow',
-                'group-hover/root:opacity-100 group-hover/item:text-foreground',
-                actives.value.includes(item.id)
-                  ? 'font-bold opacity-100 text-foreground'
-                  : 'opacity-0',
-              ]}
-              title={item.text}
-              v-html={item.text}
-            />
-          </a>
-          {renderToc(item.children, level + 1)}
-        </li>
-      ))}
+            >
+              {/* prefix */}
+              <span
+                class={[
+                  // mobile
+                  'hidden',
+                  // lg
+                  'lg:block h-1 bg-foreground rounded-sm opacity-30 transition-300 shrink-0',
+                  'group-hover/item:opacity-85',
+                ]}
+                style={{
+                  width: `${20 - level * 4}px`,
+                  marginRight: `${32 - (6 - level) * 4}px`,
+                }}
+              />
+              <span
+                class={[
+                  // normal
+                  isActive ? 'font-bold text-foreground' : '',
+                  // mobile
+                  'opacity-100',
+                  'text-[color-mix(in_srgb,var(--foreground)_70%,transparent)] text-sm overflow-hidden text-ellipsis whitespace-nowrap transition-300',
+                  'max-w-full flex-grow',
+                  // lg
+                  'lg:opacity-0 lg:max-w-46',
+                  'group-hover/root:opacity-100 group-hover/item:text-foreground',
+                  isActive ? 'lg:opacity-100' : 'lg:opacity-0',
+                ]}
+                title={item.text}
+                v-html={item.text}
+              />
+            </a>
+            {renderToc(item.children, level + 1)}
+          </li>
+        );
+      })}
     </ul>
   );
 };
